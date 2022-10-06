ca91fc25b555e203cb523af161302f7821c26eff
salmakhatun12345 <ummesalma0056@gmail.com>
2022-10-06 22:55:19 +0600

    error solving

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ function App() {
     const postLoad = async () => {
       const res = await axios.post('http://demo2211087.mockable.io/mock')
       setPost(res?.data)
-      console.log(res.data)
     }
     postLoad();
 
   }, [])
-  console.log(post?.companies)
 
   return (
     <div className="overflow-x-auto relative shadow-md sm:rounded-lg w-1/2 mx-auto mt-12">
@@ -21,9 +19,6 @@ function App() {
         <tbody>
           {post?.companies?.map((user, i) => (
             <tr key={i} className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-600">
-              <th scope="row" className="py-4 pl-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                <input checked id="checked-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-              </th>
               <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 {user.name}
               </th>
@@ -31,8 +26,8 @@ function App() {
                 {user.email}
               </td>
               <td className="py-4 px-6">
-                <span class="tracking-wider  border text-black px-2 border-rounded py-1 text-sm rounded leading-loose mx-2 font-semibold" title="">
-                  <i class="fas fa-circle text-blue-700" aria-hidden="true"></i> {user.status}
+                <span className="tracking-wider  border text-black px-2 border-rounded py-1 text-sm rounded leading-loose mx-2 font-semibold" title="">
+                  <i className="fas fa-circle text-blue-700" aria-hidden="true"></i> {user.status}
                 </span>
               </td>
             </tr>
